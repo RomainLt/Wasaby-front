cff276a29dcb60798762e92c75e3490d6d739b76
RomainLt <zefaster.romain@gmail.com>
2017-05-11 17:13:18 +0200

    Add popup to markers

--- public/js/controllers/appController.js
+++ public/js/controllers/appController.js
@@ -1,18 +1,35 @@
-wasaby.controller('appController', function ($scope, appFactory, uiGmapGoogleMapApi) {
+wasaby.controller('appController', function($scope, appFactory, uiGmapGoogleMapApi) {
 
-    $scope.list = [];
-    
-    appFactory.loadBuoys().then(function(data){
-        $scope.markers = data;
+    appFactory.loadBuoys().then(function(data) {
+        $scope.map.markers = data;
     });
-    
+
+    appFactory.loadBuoysAll().then(function(data) {
+        $scope.zobe = data;
+    });
+
     $scope.map = {
         center: {
             latitude: 48.4,
             longitude: -4.4833
         },
         zoom: 2,
+        markers: [], // Markers here
+        markersEvents: {
+            click: function(marker, eventName, model) {
+                $scope.map.window.model = model;
+                $scope.map.window.show = true;
+            }
+        },
+        window: {
+            marker: {},
+            show: false,
+            closeClick: function() {
+                this.show = false;
+            },
+            options: {}
+        }
     };
-    
-    uiGmapGoogleMapApi.then(function (maps) {});
+
+    uiGmapGoogleMapApi.then(function(maps) {});
 });
--- public/js/factories/appFactory.js
+++ public/js/factories/appFactory.js
@@ -1,14 +1,23 @@
-wasaby.factory('appFactory', function ($http) {
-    
+wasaby.factory('appFactory', function($http) {
+
     var buoysJson = "../../resources/buoys.json";
 
     return {
-        loadBuoys: function(){
+        loadBuoys: function() {
             return $http({
                 method: 'GET',
                 url: buoysJson,
-                headers: {'Content-Type': 'application/json'}
-            }).then(function(response){
+                headers: { 'Content-Type': 'application/json' }
+            }).then(function(response) {
+                return response.data;
+            });
+        },
+        loadBuoysAll: function() {
+            return $http({
+                method: 'GET',
+                url: "http://wasabyback.cleverapps.io/search/",
+                headers: { 'Content-Type': 'application/json' }
+            }).then(function(response) {
                 return response.data;
             });
         },
